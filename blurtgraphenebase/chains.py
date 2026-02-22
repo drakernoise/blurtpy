8d31f239e25f6b5f0d15f65af6f8bfc0941cc232
drakernoise <drakernoise2013@gmail.com>
2026-02-22 23:24:20 +0100

    Fix Blurt Mainnet chain_id and prefix BLT

--- blurtgraphenebase/chains.py
+++ blurtgraphenebase/chains.py
@@ -2,11 +2,21 @@
 default_prefix = "BLURT"
 known_chains = {
     "BLURT": {
-        "chain_id": "cd8d90f29f399dd4b78908d4422042531c353326660f97d9f96669084a973210",
-        "prefix": "BLURT",
+        "chain_id": "cd8d90f29ae273abec3eaa7731e25934c63eb654d55080caff2ebb7f5df6381f",
+        "prefix": "BLT",
         "chain_assets": [
-            {"asset": "BLURT", "symbol": "BLURT", "precision": 3, "id": 1},
-            {"asset": "VESTS", "symbol": "VESTS", "precision": 6, "id": 2}
+            {"asset": "@@000000021", "symbol": "BLURT", "precision": 3, "id": 0},
+            {"asset": "@@000000021", "symbol": "BLURT", "precision": 3, "id": 1},
+            {"asset": "@@000000037", "symbol": "VESTS", "precision": 6, "id": 2}
+        ],
+    },
+    "blurt": {
+        "chain_id": "cd8d90f29ae273abec3eaa7731e25934c63eb654d55080caff2ebb7f5df6381f",
+        "prefix": "BLT",
+        "chain_assets": [
+            {"asset": "@@000000021", "symbol": "BLURT", "precision": 3, "id": 0},
+            {"asset": "@@000000021", "symbol": "BLURT", "precision": 3, "id": 1},
+            {"asset": "@@000000037", "symbol": "VESTS", "precision": 6, "id": 2}
         ],
     },
     "TESTNET": {
